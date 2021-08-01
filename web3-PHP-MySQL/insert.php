2d7e04c1542f6939d030fed561e59019f0289c78
Jinha Hwang <43601998+pidokige02@users.noreply.github.com>
2021-08-01 21:28:44 +0900

    Add files via upload

---
+++ web3-PHP-MySQL/insert.php
@@ -0,0 +1,18 @@
+<?php
+$conn = mysqli_connect("localhost", "pidokige", "Feb02pid~", "opentutorials");
+$sql  = "
+	INSERT INTO topic (
+		title,
+		description,
+		created
+	) VALUES (
+		'MySQL',
+		'MySQL is ....',
+		NOW()
+	)";
+$result = mysqli_query($conn, $sql);
+if($result === false){
+    echo mysqli_error($conn);
+}
+
+?>
